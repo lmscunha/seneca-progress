4bf079ee2691403cc61cb0206f4c01e9f1bd232f
Leonardo Cunha <agile.ice4816@fastmail.com>
2026-02-28 09:35:41 -0300

    chore: update dependencies version

--- dist/ProgressDoc.d.ts
+++
@@ -1,2 +0,0 @@
-declare const docs: {};
-export default docs;
--- dist/ProgressDoc.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/* Copyright © 2023 Seneca Project Contributors, MIT License. */
-Object.defineProperty(exports, "__esModule", { value: true });
-const docs = {};
-exports.default = docs;
-if ('undefined' !== typeof module) {
-    module.exports = docs;
-}
-//# sourceMappingURL=ProgressDoc.js.map
